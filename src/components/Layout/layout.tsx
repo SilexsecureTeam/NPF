69450fe4c8b58e9741ec5785bda916fecb38bcd8
Megabyte-webdev <afolabimubarak18@gmail.com>
2025-07-28 22:15:21 +0100

    chatbot added

--- src/components/Layout/layout.tsx
+++ src/components/Layout/layout.tsx
@@ -1,21 +1,22 @@
-import { ReactNode} from "react";
+import { ReactNode } from "react";
 import Header from "./header/header";
 import Footer from "./footer/footer";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 
 type Props = {
   children: ReactNode;
 };
 
 export const Layout = ({ children }: Props) => {
-
   return (
     <>
       <Header />
-      <main>
-        {children}
-      </main>
-      <Footer/>
-     
+      <main>{children}</main>
+      <TawkMessengerReact
+        propertyId="6887dd3d670c54192a8b455b"
+        widgetId="1j19bgefu"
+      />
+      <Footer />
     </>
   );
 };
